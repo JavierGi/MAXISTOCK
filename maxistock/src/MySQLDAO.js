807772bc2c51a75e6fde1e4f575159fdf3857cdf
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2021-09-15 17:23:21 -0300

    Creacion de react app, mysql, tests mysql

---
+++ maxistock/src/MySQLDAO.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+});
+
+export const connect = () => connection.connect();
+
+export const query = (sql) => {
+    return new Promise( (resolve, reject) => {
+        connection.query(sql, (err, rows) =>{
+            if (err) return reject(err);
+            resolve(rows)
+        });
+    });
+}
+
+export const disconnect = () => connection.end();
+
