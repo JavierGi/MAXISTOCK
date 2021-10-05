679c97b2717169751e5f9635228be203902675d7
Lautaro <lautaro.ml@gmail.com>
2021-10-05 15:48:39 -0300

    Terminada Tabla de contenidos

---
+++ maxistock/src/services/ContentsTableOrquestrator.js
@@ -0,0 +1,15 @@
+import {query, connect, disconnect} from "../dao/MySQLDAO";
+
+function GetEverything() {
+    connect
+    query("SELECT * FROM stock")
+    disconnect
+}
+
+function GetMostBuyed() {
+    connect
+    query("SELECT nombre, cantidad, precio FROM stock JOIN (SELECT * FROM ventas ORDER BY cantidad) ON codigo_producto = codigo")
+}
+
+export default GetEverything;
+export default GetMostBuyed;
