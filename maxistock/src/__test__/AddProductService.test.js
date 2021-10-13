a465c704bc4227964653ded70625d1a78e4b2448
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2021-10-13 18:50:33 -0300

    8 - Test unitarios AddProductService

--- maxistock/src/__test__/AddProductService.test.js
+++ maxistock/src/__test__/AddProductService.test.js
@@ -1,7 +1,34 @@
 import AddProductService from "../services/AddProductService";
 
-test('added product to db', () => {
 
+beforeAll(() => {
+});
+
+beforeEach(() => {
+});
+
+afterEach(() => {
+});
+
+afterAll(() => {
+});
+
+test('AddProductService add a product', () => {
+    const AddProductDAO = jest.fn();
+    const AddProductService_B = AddProductDAO.bind(AddProductService);
+
+    AddProductService_B("Cosa", 1, 1);
+
+    expect(AddProductDAO.mock.calls.length).toBe(1)
+});
+
+test('AddProductService create a product', () => {
+    const NewProductDTO = jest.fn();
+    const AddProductService_B = NewProductDTO.bind(AddProductService);
+
+    AddProductService_B("Cosa", 1.0, 1);
+
+    expect(NewProductDTO.mock.calls.length).toBe(1) 
 });
 
 test('price is too low', () => {
