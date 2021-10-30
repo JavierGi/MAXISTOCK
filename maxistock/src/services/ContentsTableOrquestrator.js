da30dbd5cbc07c116106459bd10b70b9b5bb739a
Lautaro <lautaro.ml@gmail.com>
2021-10-30 15:22:21 -0300

    9 - arreglado error de import en ContentsTableOrquestrator

--- maxistock/src/services/ContentsTableOrquestrator.js
+++ maxistock/src/services/ContentsTableOrquestrator.js
@@ -1,18 +1,17 @@
-import {query, connect, disconnect} from "../dao/MySQLDAO";
-import { stock , getProductBycode } from '../dao/StockDAO';
+import { stock , getProductByCode } from '../dao/StockDAO';
 
-function GetEverything() {
+export function GetEverything() {
   return stock;
 };
 
-export const GetMostBuyed = () => {
+/*function GetMostBuyed() {
     connect()
     query("SELECT nombre, cantidad, precio FROM stock JOIN (SELECT * FROM ventas ORDER BY cantidad) ON codigo_producto = codigo")
     disconnect()
-};
+};*/
 
-function GetProductByCode(code) {
+export function GetProductByCode(code) {
     return getProductByCode(code);
 };
 
-module.exports = {GetEverything, GetMostBuyed, GetProductByCode};
+//module.exports = {GetEverything, GetProductByCode};
