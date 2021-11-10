457d576d49279201e6e9a1e6b690ffa1e1d95f44
Lautaro <lautaro.ml@gmail.com>
2021-11-10 19:55:01 -0300

    19 - arreglado bug no muestra cantidad vendida en pantalla

--- maxistock/src/services/ContentsTableOrquestrator.js
+++ maxistock/src/services/ContentsTableOrquestrator.js
@@ -11,7 +11,7 @@ function GetMostBuyed() {
 
 //Nahue, como la otra vez, vos conectá tu query a estas funciones así no tenés que tocar el código del front
 function GetLeastBuyed() {
-  return [{codigo: 1, nombre: "galletitas", precio: 150, cantidad: 0}, {codigo: 1, nombre: "agua", precio: 100, cantidad: 0}, {codigo: 1, nombre: "jugo", precio: 210, cantidad: 5}]
+  return [{codigo: 1, nombre: "galletitas", precio: 150, ventas: 0}, {codigo: 1, nombre: "agua", precio: 100, ventas: 0}, {codigo: 1, nombre: "jugo", precio: 210, ventas: 5}]
 }
 
 //Nahue, como la otra vez, vos conectá tu query a estas funciones así no tenés que tocar el código del front
