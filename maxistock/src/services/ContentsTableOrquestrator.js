25dd1af492bfceb019fe9e022e16dc68d12b8e7b
NahuelIglesias <nahueliglesias1999@gmail.com>
2021-11-10 21:41:15 -0300

    18 - 71 - conexión de front con back para utilizar nuevo service

--- maxistock/src/services/ContentsTableOrquestrator.js
+++ maxistock/src/services/ContentsTableOrquestrator.js
@@ -1,6 +1,7 @@
 import { stock , getProductByCode } from '../dao/StockDAO';
 import GetMostSoldProducts from "../services/GetMostSoldProducts";
 import GetLeastSoldProductsService from "../services/GetLeastSoldProductsService";
+import OutOfStockProductsService from "../services/OutOfStockProductsService";
 
 function GetEverything() {
   return stock;
@@ -14,9 +15,8 @@ function GetLeastBuyed() {
   return GetLeastSoldProductsService();
 }
 
-//Nahue, como la otra vez, vos conectá tu query a estas funciones así no tenés que tocar el código del front
 function GetSoldOutProducts() {
-  return [{codigo: 1, nombre: "galletitas", precio: 150, cantidad: 0}, {codigo: 1, nombre: "agua", precio: 100, cantidad: 0}, {codigo: 1, nombre: "jugo", precio: 210, cantidad: 0}]
+  return OutOfStockProductsService();
 }
 
 function GetProductByCode(code) {
